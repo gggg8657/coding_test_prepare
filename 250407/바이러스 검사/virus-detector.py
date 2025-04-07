e0064bf2c5b3b149cc9b471456d2a19c0f5478ad
DongJu Kim <gggg8657@gmail.com>
2025-04-07 13:25:29 +0900

    Title: 바이러스 검사, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250407/바이러스 검사/virus-detector.py
+++ 250407/바이러스 검사/virus-detector.py
@@ -8,7 +8,7 @@ result = 0
 for i in range(N):
     res = array[i] - C
     result += 1
-    if res>=0:
+    if res>0:
         if res - (m * res//m) >= 0:
             result+=1
         else : result+=0
