96949684bdc556d091107a0e65067dc9007eb37f
DongJu Kim <gggg8657@gmail.com>
2025-04-07 13:24:15 +0900

    Title: 바이러스 검사, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250407/바이러스 검사/virus-detector.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+array = list(map(int, input().split()))
+
+C, m = map(int, input().split())
+
+result = 0
+for i in range(N):
+    res = array[i] - C
+    result += 1
+    if res>=0:
+        if res - (m * res//m) >= 0:
+            result+=1
+        else : result+=0
+        result += res//m
+
+print(result)
