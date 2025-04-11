5263d5494e3b37d8526ef076a7c4104b52a90d4f
DongJu Kim <gggg8657@gmail.com>
2025-04-11 19:41:07 +0900

    Title: 원자 충돌, Time: 318ms, Memory: 30MB, Status: Passed - Codetree

--- 250411/원자 충돌/atom-collision.py
+++ 250411/원자 충돌/atom-collision.py
@@ -1,36 +1,18 @@
+from collections import deque, defaultdict
+import sys
+input = sys.stdin.readline
+
 class atom:
-    def __init__(self):
+    def __init__(self, id):  # 고유 ID 부여
+        self.id = id
         self.r = 0
         self.c = 0
         self.m = 0
         self.s = 0
         self.d = 0
 
-import copy
-import sys
-input = sys.stdin.readline
-# def check(cq):
-#     clpsed = deque()
-#     tmp = deque()
-#     for i in range(len(cq)):
-#         src = cq[i]
-#         clpsed = deque()
-#         for j in range(len(cq)):
-#             tar = cq[j]
-#             if src.r == tar.r and src.c == tar.c: # 겹쳤으면, 
-#                 clpsed.append(tar)
-#         if clpsed not in tmp and len(clpsed)>=2: 
-#             tmp.append(clpsed)
-#     if len(tmp)<=0: 
-#         return _, False
-#     else : 
-#         return tmp, True
-
-from collections import defaultdict
-
 def check(cq):
     pos_dict = defaultdict(deque)
-
     for atom in cq:
         pos_dict[(atom.r, atom.c)].append(atom)
 
@@ -39,112 +21,83 @@ def check(cq):
         if len(atoms) >= 2:
             tmp.append(atoms)
 
-    return (tmp, True) if tmp else (_, False)
+    return tmp, True if tmp else (_, False)
 
-def Divide(tq,mem):
-    # del_list = []
-    tmp = deque()
+def Divide(tq_dict, mem, unique_id):
+    new_atoms = deque()
+
     while mem:
-        tmp=mem.popleft()
+        tmp = mem.popleft()
         total_m = 0
         total_s = 0
         direction = []
+        r = c = -1
+        ids_to_remove = []
+
         while tmp:
             atom_n = tmp.popleft()
-            tq.remove(atom_n)
+            ids_to_remove.append(atom_n.id)
             total_m += atom_n.m
-            
-            direction.append(abs(atom_n.d %2))
             total_s += atom_n.s
+            direction.append(atom_n.d % 2)
+            r, c = atom_n.r, atom_n.c  # 위치 기록
 
-        #summation done
-        # new m
-        new_m = total_m//5 
-        if new_m == 0: 
-            continue    #질량 나눴더니 0 됨 원자 소멸
-                        # 원래 있던 q 에서도 날려줘야 되는데 어쩌노 이미 popleft 하면서 뺴고, 분리 하고 다시 넣으므로 무관
-        else : 
-            a, b, c ,d = atom(), atom(), atom(), atom()
-            a.m = b.m = c.m = d.m = new_m
+        for _id in ids_to_remove:
+            if _id in tq_dict:
+                tq_dict.pop(_id)
 
-        # new direction
-        if min(direction) != max(direction): # 배열 안에 0 과 1 이 모두 있음
-            a.d, b.d, c.d, d.d = 1, 3, 5, 7
-        else : 
-            a.d, b.d, c.d, d.d = 0, 2, 4, 6 #한가지 종류로 됨
+        new_m = total_m // 5
+        if new_m == 0:
+            continue
 
-        # new speed
-        new_s = total_s // len(direction) # 합쳐진 원자의 개수
-        a.s = b.s = c.s = d.s = new_s
+        new_s = total_s // len(direction)
+        dirs = [1, 3, 5, 7] if min(direction) != max(direction) else [0, 2, 4, 6]
 
-        # new r, c
-        a.r = b.r = c.r = d.r = atom_n.r
-        a.c = b.c = c.c = d.c = atom_n.c
-        q.append(a)
-        q.append(b)
-        q.append(c)
-        q.append(d)
-        
-    return tq,q
-        
+        for d in dirs:
+            a = atom(unique_id)
+            unique_id += 1
+            a.r, a.c = r, c
+            a.m = new_m
+            a.s = new_s
+            a.d = d
+            new_atoms.append(a)
 
-        
-n , m , k = map(int, input().split())
+    return tq_dict, new_atoms, unique_id
 
-from collections import deque
-
+# 입력 처리
+n, m, k = map(int, input().split())
 dr, dc = [-1,-1,0,1,1,1,0,-1], [0,1,1,1,0,-1,-1,-1]
 
 q = deque()
-for _ in range(m):
-    a = atom()
+for i in range(m):
+    a = atom(i)
     a.r, a.c, a.m, a.s, a.d = map(int, input().split())
     q.append(a)
 
+unique_id = m  # 새 원자에 부여할 고유 ID 시작
+
+# 시뮬레이션 시작
 for _ in range(k):
-    tmpq = deque() #이동된 원자 위치 저장할 q 초기화
+    tmpq = deque()
     while q:
         ca = q.popleft()
         nr = (ca.r + ca.s * dr[ca.d]) % n
         nc = (ca.c + ca.s * dc[ca.d]) % n
         ca.r = nr
         ca.c = nc
-        tmpq.append(ca) #이동 후 원자의 위치 저장
-        # if 0<=nr<n and 0<=nc<n:
-        #     ca.r = nr
-        #     ca.c = nc
-        #     tmpq.append(ca)
+        tmpq.append(ca)
 
-    mem, is_collapse = check(tmpq) # 합쳐진거 있는지 확인 mem은 deque로 구성 : 합쳐진 것들의 집합을 mem 에 포함 즉 mem.popleft() -> 합쳐진거 집합 1 mem.popleft().popleft() -> 합쳐진거 집합 1의 원자 하나
-    if is_collapse:  # 합쳐진게 있음
-        tmpq,q = Divide(tmpq, mem)  # update q and does not return tmpq 안에 있는거도 날려줘야됨
+    mem, is_collapse = check(tmpq)
+    tmpq_dict = {a.id: a for a in tmpq}
 
-    for item in tmpq: q.append(item) #이동된 원자의 위치 q에 추가
-    # tmpq 에 사라진 원소도 남아있음...
+    if is_collapse:
+        tmpq_dict, new_atoms, unique_id = Divide(tmpq_dict, mem, unique_id)
+        for a in new_atoms:
+            q.append(a)
 
+    for a in tmpq_dict.values():
+        q.append(a)
 
-
-res = 0
-for ato in q:
-    res += ato.m
-
+# 질량 합산 결과 출력
+res = sum(a.m for a in q)
 print(res)
-'''
-init -> 1sec -> if check(q) == True : # 합쳐진게 있음
-                    원자분해 -> 4개로 ()
-
-원자분해 -> 4개로 (합쳐진 원자들):
-    질량 = (합쳐진 질량)//5
-    if 질량 == 0 -> del 합쳐진 원자들
-    else:
-        속력 = 합쳐진 원자 속력 / 합쳐진 원자 개수
-        if 합쳐진 원자 상 하 좌 우 || 대각 대각 대각 대각 :
-            방향 = 상하좌우
-        else : 방향 = 4대각
-        
-        q. append(원자분해된 4개의 원자) 
-        
-    
-                        
-'''
-
